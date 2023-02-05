b732e1f38f544305f0ffe88a0898639c45e8a6f1
Metin Emre <54211029+metinemredonmez@users.noreply.github.com>
2023-02-05 19:36:50 +0300

    Initial Commit

---
+++ navigationUI/navigationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationUIApp.swift
+//  navigationUI
+//
+//  Created by emre on 5.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct navigationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
